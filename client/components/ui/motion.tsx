8280aeb0b96ac23948dffe68c2eb6d46eb380d3d
Devesh Ghuge <deveshghuge1676@gmail.com>
2025-10-17 21:27:21 +0000

    Add MotionWrapper component
    cgen-e8cd4a71a773446ab4f9188bd91d882f

---
+++ client/components/ui/motion.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import { motion } from "framer-motion";
+import type { HTMLMotionProps } from "framer-motion";
+
+interface MotionWrapperProps extends HTMLMotionProps<"div"> {
+  children: React.ReactNode;
+}
+
+export const MotionWrapper: React.FC<MotionWrapperProps> = ({ children, ...props }) => {
+  return (
+    <motion.div {...props}>
+      {children}
+    </motion.div>
+  );
+};
+
+export default MotionWrapper;
